835ec3d5677ea4fff8beda9b903f2c734c1a95ae
rbettN <bettoni.rafa@gmail.com>
2021-02-17 22:16:31 -0500

    lesson145+: Implemented a SideDrawer in the Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,10 +6,12 @@ import React from 'react';
 import Aux from '../../highordercomponents/Auxiliary'
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/Sidedrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar/>
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
