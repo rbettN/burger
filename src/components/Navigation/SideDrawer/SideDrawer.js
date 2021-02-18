9c5a20340e53d221a4bd15e47483d1efb79cd4ea
rbettN <bettoni.rafa@gmail.com>
2021-02-17 23:00:23 -0500

    lesson147: Responsive Adjustments

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,7 +7,9 @@ const sideDrawer = (props) => {
     //...
     return(
         <div className={classes.sideDrawer}>
-            <Logo/>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
             <nav>
                 <NavigationItems/>
             </nav>
