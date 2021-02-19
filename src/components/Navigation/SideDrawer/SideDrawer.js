4569059bde05c471ef7502acfc36c6dcbe42fd71
rbettN <bettoni.rafa@gmail.com>
2021-02-18 21:06:09 -0500

    lesson148: Responsive Adjustments - SideDrawer and Toolbar

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,7 +6,7 @@ import classes from './SideDrawer.css';
 const sideDrawer = (props) => {
     //...
     return(
-        <div className={classes.sideDrawer}>
+        <div className={classes.SideDrawer}>
             <div className={classes.Logo}>
                 <Logo/>
             </div>
