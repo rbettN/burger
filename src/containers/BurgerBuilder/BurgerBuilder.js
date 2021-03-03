9633a2e55e32f5ece723e934fe40f775fb551f0f
rbettN <bettoni.rafa@gmail.com>
2021-03-02 21:53:27 -0500

    lesson183: Implemented an error handler component that wrapps the BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,6 +11,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../highordercomponents/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 
 const INGREDIENTS_PRICES = {
@@ -147,4 +148,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
