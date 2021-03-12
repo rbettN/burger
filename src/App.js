196900d99e2278e76fc53b6343a07b2fb3cce062
rbettN <bettoni.rafa@gmail.com>
2021-03-11 21:43:30 -0500

    lesson222: Added BrowserRouter to index.js and Routes to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
  */
 
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './highordercomponents/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -12,8 +13,10 @@ class App extends Component {
       return (
         <div >
           <Layout>
-          <BurgerBuilder/>
-          <Checkout/>
+            <Switch>
+              <Route path="/checkout" component={Checkout}/>  
+              <Route path="/" exact component={BurgerBuilder}/>
+            </Switch>
           </Layout>
         </div>
       );
