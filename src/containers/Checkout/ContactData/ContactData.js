7034733b6ed34f70c41948c70c88139f5bcac2a4
rbettN <bettoni.rafa@gmail.com>
2021-04-14 21:30:27 -0400

    Lesson667: Working on Order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,8 +92,7 @@ class ContactData extends Component {
                 valid: true
             }
         },
-        formIsValid: false,
-        loading: false
+        formIsValid: false
     }
 
     orderHandler = (event) => {
@@ -170,7 +169,7 @@ class ContactData extends Component {
                 <Button btnType="Sucess" disabled={!this.state.formIsValid} clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner />;
         }
         return (
@@ -185,13 +184,16 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrive
+        price: state.totalPrive,
+        loading: state.loading
     }
 };
 
 const mapDispatchToProps = dispatch => {
-    onOrderBurger: (orderData) => dispatch (actions.purchaseBurgerStart(orderData))
-}
+    return {
+        onOrderBurger: (orderData) => dispatch (actions.purchaseBurger(orderData))
+    };
+};
 
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
