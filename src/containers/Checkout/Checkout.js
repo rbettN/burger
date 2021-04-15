42e71e1592c7d56975a986e8b6498ec36e6042a2
rbettN <bettoni.rafa@gmail.com>
2021-04-14 21:36:42 -0400

    Lesson668: Used Redirect to improve UX

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,7 +3,7 @@
  */
 
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 import { connect } from 'react-redux';
@@ -15,8 +15,10 @@ class Checkout extends Component {
     }
 
     render () {
-        return (
-            <div>
+        let summary = <Redirect to="/"/>
+        if(this.props.ings) {
+            summary = (
+                <div>
                 <CheckoutSummary 
                     ingredients={this.props.ings}
                     checkoutCancelled={this.checkoutCancelledHandler}
@@ -24,8 +26,10 @@ class Checkout extends Component {
                 <Route 
                     path={this.props.match.path + '/contact-data/'} 
                     component={ContactData} />
-            </div>
-        );
+                </div>    
+            );
+        }
+        return summary;
     }
 }
 
