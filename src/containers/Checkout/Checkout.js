7a7c03ffca699f40ff0e5d093021ca9026973eb1
rbettN <bettoni.rafa@gmail.com>
2021-04-18 18:04:48 -0400

    Lesson670-671: Combined Reducers and handled purchases

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -7,9 +7,10 @@ import { Route, Redirect } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
-    
+
     checkoutContinuedHandler = () => {
         this.props.history.replace('/checkout/contact-data');
     }
@@ -17,15 +18,17 @@ class Checkout extends Component {
     render () {
         let summary = <Redirect to="/"/>
         if(this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/"/> : null;
             summary = (
                 <div>
-                <CheckoutSummary 
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}/>
-                <Route 
-                    path={this.props.match.path + '/contact-data/'} 
-                    component={ContactData} />
+                    {purchasedRedirect}
+                    <CheckoutSummary 
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}/>
+                    <Route 
+                        path={this.props.match.path + '/contact-data/'} 
+                        component={ContactData} />
                 </div>    
             );
         }
@@ -35,7 +38,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 };
 
