c631292ccd945673b919bf9a2b1453395bde37c7
rbettN <bettoni.rafa@gmail.com>
2021-04-21 21:42:25 -0400

    Lesson696-697: Added guards to routes in the App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
  */
 
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from './highordercomponents/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -20,21 +20,41 @@ class App extends Component {
     }
 
     render() {
+      let routes = (
+        <Switch>
+          <Route path="/auth" component={Auth}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+        </Switch>
+      );
+
+      if (this.props.isAuthenticated) {
+        routes = (
+          <Switch>
+            <Route path="/checkout" component={Checkout}/>  
+            <Route path="/orders" component={Orders}/>
+            <Route path="/logout" component={Logout}/>
+            <Route path="/" exact component={BurgerBuilder}/>
+          </Switch>
+        );
+      }
+
       return (
         <div >
           <Layout>
             <Switch>
-              <Route path="/checkout" component={Checkout}/>  
-              <Route path="/orders" component={Orders}/>
-              <Route path="/auth" component={Auth}/>
-              <Route path="/logout" component={Logout}/>
-              <Route path="/" exact component={BurgerBuilder}/>
+              {routes}
             </Switch>
           </Layout>
         </div>
       );
     }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
