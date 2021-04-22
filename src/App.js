2bc20c60aa11ee7e875dfbd54681d6201150e372
rbettN <bettoni.rafa@gmail.com>
2021-04-21 20:38:28 -0400

    Lesson694: Added persistent authentication through LocalStorage

--- src/App.js
+++ src/App.js
@@ -10,8 +10,15 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
 
 class App extends Component {
+
+    componentDidMount () {
+      this.props.onTryAutoSignup();
+    }
+    
     render() {
       return (
         <div >
@@ -29,4 +36,10 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
