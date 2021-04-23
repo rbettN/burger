7f83d9efd2d667188fdc0286fd6d81cdf9cdfdd5
rbettN <bettoni.rafa@gmail.com>
2021-04-22 20:31:57 -0400

    Lesson701-702.4: Moved the utility.js and used it for Auth and ContactData components

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -11,6 +11,7 @@ import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import { Redirect } from 'react-router-dom';
+import { updateObject } from '../../shared/utility';
 
 class Auth extends Component {
     state = {
@@ -68,15 +69,13 @@ class Auth extends Component {
     }
 
     inputChangeHandler = (event, controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
+        const updatedControls = updateObject(this.state.controls, {
+            [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
                 valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
-            }
-        };
+            })
+        });
         this.setState({controls: updatedControls});
     }
 
